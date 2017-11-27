39a8994519e21d9557b7f9a645b34b9a0fa98760
Zachary Robertson <ztrsec@hotmail.com>
2017-11-27 14:27:47 +1100

    Added Scenes, new Prefabs and Test Scenes

---
+++ Assets/!Test/Scripts/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Test : MonoBehaviour
+{
+    public float checkDelay = 0f;
+    private float checkTimer = 0f;
+
+    protected virtual void Simulate() {}
+    protected virtual void Debug() {}
+    protected abstract void Check();
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Call simulate
+        Simulate();
+        //Check on interval (0 if once per frame)
+        checkTimer += Time.deltaTime;
+        if(checkTimer >= checkDelay)
+        {
+            Check();
+        }
+        //Perform debugging
+        Debug();
+    }
+}
---
+++ Assets/!Test/Scripts/Test_Jump.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Jump : Test
+{
+    [Header("Test Parameters")]
+    public float minHeight = 1f;
+
+    private PlayerController player;
+    private float originalY;
+    private float jumpApex;
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+        //Set our original Y
+        originalY = transform.position.y;
+    }
+    protected override void Simulate()
+    {
+        //simulate Jump
+        player.Jump();
+    }
+    protected override void Check()
+    {
+        //Get the currnet player y position
+        float playerY = transform.position.y;
+        float height = playerY - originalY;
+        //If the height is over the jumpApex
+        if(height > jumpApex)
+            jumpApex = height;
+        //If the jumpApex is higher than minHeight
+        if(jumpApex > minHeight)
+            IntegrationTest.Pass(gameObject);
+    }
+    
+}
